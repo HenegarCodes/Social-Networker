5c89a822731ef7a597b46235b79a6c80228c5789
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-01-08 17:54:59 -0700

    adding the mongoose connection

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect('mongodb+srv://admin:Password123!@cluster0.53nmbtc.mongodb.net/?retryWrites=true&w=majority');
+
+// Export connection 
+module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -13,4 +13,4 @@ db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
-});
+}); 
